e98a398213c2396a0b55dd332fbfe6a27ac77f7f
Artsem Sauchuk <artsem.sauchuk@autenti.com>
2020-01-05 11:46:12 +0100

    enadle spring debug

--- src/main/java/com/artemmensk/next4free/application/ProcessNotFound.java
+++ src/main/java/com/artemmensk/next4free/application/ProcessNotFound.java
@@ -3,9 +3,9 @@ package com.artemmensk.next4free.application;
 import com.artemmensk.next4free.domain.BusinessId;
 import com.artemmensk.next4free.domain.ClientId;
 
-public class ProcessNotFound extends RuntimeException {
+class ProcessNotFound extends RuntimeException {
 
-    public ProcessNotFound(
+    ProcessNotFound(
             ClientId clientId,
             BusinessId businessId) {
         super(String.format("Not found process for %s and %s", clientId, businessId));
